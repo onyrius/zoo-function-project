4add7fb75dcdcb5a119be52259277628729e1557
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-08 17:09:57 +0100

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
+
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const animais = [];
+  species.filter((animalObjeto) => ids.map((id) => {
+    if (animalObjeto.id === id) {
+      animais.push(animalObjeto);
+    }
+    return animais;
+  }));
+  return animais;
 }
 
 module.exports = getSpeciesByIds;
