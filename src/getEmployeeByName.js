896a8552fda31d3af15999d87f3dbf2997d6d991
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-09 19:51:55 +0100

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const { employees } = data;
+  const nomeEmployee = {};
+  const findNome = (nome) => nome.firstName === employeeName || nome.lastName === employeeName;
+  const names = employees.find(findNome);
+  return Object.assign(nomeEmployee, names);
 }
 
 module.exports = getEmployeeByName;
