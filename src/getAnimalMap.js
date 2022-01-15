ccfd843266a43ef5915b06a29307c9cf88e39802
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-15 10:04:38 +0100

    ponto 5 do requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -9,72 +9,44 @@ const animalMap = species.reduce((animalLocation, nameAndLocation) => {
   if (location === 'SW') animalLocation.SW.push(name);
   return animalLocation;
 }, { NE: [], NW: [], SE: [], SW: [] });
+// console.log(animalMap);
+//-------------------------------------------------------------------------------
 
-// console.log('animalMap', animalMap);
+const findLocationSpecie = () => species.reduce((objectLocation, specie) => {
+  const { name, residents } = specie;
+  const objectSpecieNameRedidents = {};
+  objectSpecieNameRedidents[name] = residents.map((resident) => resident.name);
+  //  console.log('objeto dentro do filter nameSpeciesLocation', objectSpecieNameRedidents);
+  if (specie.location === 'NE') objectLocation.NE.push(objectSpecieNameRedidents);
+  if (specie.location === 'NW') objectLocation.NW.push(objectSpecieNameRedidents);
+  if (specie.location === 'SE') objectLocation.SE.push(objectSpecieNameRedidents);
+  if (specie.location === 'SW') objectLocation.SW.push(objectSpecieNameRedidents);
+  return objectLocation;
+}, { NE: [], NW: [], SE: [], SW: [] });
 
-// obj = { location : [ {specie.name : residents[index].name}]}
-const findSpecies = () => species.map((specie) => specie.name);
-// console.log('usando o map', findSpecies());
-
-const findLocation = (local) => species.find((animalLocation) => animalLocation.location === local); // mais condiçao para encontrar lugar;
-// console.log('usando o find para a localizacao', findLocation('NW'));
-
-// obj = { location : [ {specie.name : residents[index].name}]}
-const encontrandoSpecies = (local) => {
-  const locationAnimal = findLocation(local).location;
-  console.log('linha 25, localizaçao: ', locationAnimal);
-  console.log('---------------------------------------------');
-  const propertySpecie = findLocation(local).name;
-  console.log('linha 28,especie: ', propertySpecie);
-  console.log('---------------------------------------------');
-  const nameAnimals = findLocation(local).residents.map((namesInZoo) => namesInZoo.name);
-  console.log('linha 31, nomes: ', nameAnimals);
-  console.log('---------------------------------------------');
-  const objSpecie = {};
-  objSpecie[propertySpecie] = nameAnimals;
-  const objLocation = {};
-  objLocation[locationAnimal] = [objSpecie];
-  return objLocation;
-};
-console.log('tentando encontrar cada especie linha 39', encontrandoSpecies('NW'));
-console.log('---------------------------------------------');
-// obj = { location : [ {specie.name : residents[index].name}]}
-
-const findAnimalNames = () => {
-  const namesAnimal = species.map((specie) => {
-    const { residents } = specie;
-    const nameOfAnimalLocation = residents.map((namesOfAnimal) => namesOfAnimal.name);
-    return nameOfAnimalLocation;
-  });
-  return namesAnimal;
-};
-console.log('funcao findAnimalNames linha 51', findAnimalNames());
-console.log('---------------------------------------------');
-const objAnimalNameMap = findAnimalNames().reduce((acc, animalNames, index) => {
-  acc[findSpecies()[index]] = animalNames;
-  return acc;
-}, {});
-console.log('animais e seus nomes linha 57', objAnimalNameMap);
-console.log('---------------------------------------------');
-const objLocalAnimalNameMap = species.reduce((acc, locationAnimalNameMap, index) => {
-  const { location } = locationAnimalNameMap;
-  const key = Object.keys(objAnimalNameMap)[index];
-  const value = Object.values(objAnimalNameMap)[index];
-  const objFinal = { [location]: [{ [key]: value }] };
-  console.log('objFinal', objFinal);
-  return acc;
-}, { });
-console.log('objLocalAnimalNameMap linha 71', objLocalAnimalNameMap);
-console.log('---------------------------------------------');
+const sortingSpecie = () => species.reduce((objectLocation, specie) => {
+  const { name, residents } = specie;
+  const objectSpecieSortedNameRedidents = {};
+  const residentNames = residents.map((resident) => resident.name);
+  const residentSortedNames = residentNames.sort();
+  console.log(residentSortedNames);
+  objectSpecieSortedNameRedidents[name] = residentSortedNames;
+  console.log('objeto usando sort', objectSpecieSortedNameRedidents);
+  if (specie.location === 'NE') objectLocation.NE.push(objectSpecieSortedNameRedidents);
+  if (specie.location === 'NW') objectLocation.NW.push(objectSpecieSortedNameRedidents);
+  if (specie.location === 'SE') objectLocation.SE.push(objectSpecieSortedNameRedidents);
+  if (specie.location === 'SW') objectLocation.SW.push(objectSpecieSortedNameRedidents);
+  return objectLocation;
+}, { NE: [], NW: [], SE: [], SW: [] });
 
 function getAnimalMap(options) {
-  if (!options) return animalMap;
-  if (options.sex === 'female' && options.sorted === true) return animalMap;
-
-  if (options.includeNames === true) return 'chegarei aqui';
+  if (!options || options.sex === 'female') return animalMap;
+  if (options.sorted === true && options.includeNames === true) return sortingSpecie();
+  if (options.includeNames === true) return findLocationSpecie();
 }
 
-console.log('getAnimal sem paramentros', getAnimalMap());
+console.log('getAnimal includes true e sorted', getAnimalMap({ includeNames: true, sorted: true }));
+console.log('---------------------------------------------');
 // console.log('getAnimal sex', getAnimalMap({ includeNames: true, sex: 'female' }));
 // console.log('getAnimal', getAnimalMap({ includeNames: true, sex: 'female', sorted: true }));
 module.exports = getAnimalMap;
