94e669a43dc2c9c628ea4aaddd23d0be1ec4e4c5
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-15 20:33:33 +0100

    corrigindo o lint requisito 7

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalDatas = [];
+  const searchEmployee = employees.filter((emplooyee) => id === emplooyee.id);
+  const findResponsible = searchEmployee.find((responsibleFor) => responsibleFor.responsibleFor);
+  console.log('linha 8', findResponsible);
+  const findAnimals = species.find((specie) => findResponsible.responsibleFor.includes(specie.id));
+  console.log('animal', findAnimals);
+  const oldestAnimal = findAnimals.residents.map((animal) => {
+    if (Math.max(animal.age)) return animal;
+  });
+  console.log('oldest', oldestAnimal);
 }
 
+console.log('resultado da funçao', getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getOldestFromFirstSpecies;
