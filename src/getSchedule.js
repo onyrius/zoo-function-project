bb3e45a2f1ca1630b28c66bf3067f0fb67976d90
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-15 21:35:10 +0100

    consertando lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,19 +1,7 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const fillDays = () => {
-  const daysAndHours = data.hours;
-  console.log('linha 6', daysAndHours);
-  const addKeys = () => Object.keys(daysAndHours).reduce((acc, key, index) => {
-    const hoursArray = Object.values(daysAndHours);
-    console.log('hoursArray linha 9', hoursArray);
-    const officeTime = 'Open from  until';
-    acc[[key]] = new Object();
-    
-    return acc;
-  }, {});
-  console.log('addkeys', addKeys());
-  return addKeys();
+
 };
 
 console.log('fillDay', fillDays());
